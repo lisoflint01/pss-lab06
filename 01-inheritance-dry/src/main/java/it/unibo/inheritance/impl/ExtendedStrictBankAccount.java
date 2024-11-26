cd0d8511c7c2994d9ad6574fcfca5297e390e594
Lisandro Antonioli <lisandro.antonioli@studio.unibo.it>
2024-11-26 11:01:07 +0100

    ExtendedSyrictBankAccount Implementated

---
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,35 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount {
+    
+    private static final double TRANSACTION_FEE = 0.1;
+
+    public ExtendedStrictBankAccount(final int id, final double balance) {
+        super(id, balance);
+    }
+
+    public void withdraw(final int id, final double amount) {
+        if (isWithdrawAllowed(amount)) {
+            if (checkUser(id)) {
+                setBalance(getBalance() + amount);
+                this.incrementTransactions();
+            }
+        }
+    }
+
+    public void withdrawFromATM(final int id, final double amount) {
+        this.withdraw(id, amount + ExtendedStrictBankAccount.ATM_TRANSACTION_FEE);
+    }
+
+    private boolean isWithdrawAllowed(final double amount) {
+        return super.getBalance() >= amount;
+    }
+
+    public void chargeManagementFees(final int id) {
+        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * ExtendedStrictBankAccount.TRANSACTION_FEE;
+        if (checkUser(id) && isWithdrawAllowed(feeAmount)) {
+            setBalance(getBalance() - feeAmount);
+        }
+    }
+
+}
